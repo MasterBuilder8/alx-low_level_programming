a79adc854a4fdb2742a9d4e350465a55273fd163
MasterBuilder8 <collins.snilloc9@gmail.com>
2022-12-19 11:28:03 -0800

    task nine

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strcpy - copy the string pointed to by src to dest
+ * _strcpy - copy the string pointed to by src to dest
  * @dest: char to check
  * @src: char to check
  * Return: 0
@@ -11,6 +11,6 @@ char *_strcpy(char *dest, char *src)
 
 	for (b = 0; src[b] != '\0'; b++)
 		dest[b] = src[b];
-	dest[a] = '\0';
+	dest[b] = '\0';
 	return (dest);
 }
