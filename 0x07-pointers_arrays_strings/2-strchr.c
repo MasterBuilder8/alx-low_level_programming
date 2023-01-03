87717b0879eeccdeceac14b60aad0c3146301b88
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-01-03 07:01:38 -0800

    task two

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
-		if (*s == c)
-		{
-			return (s);
-
-		}
-		else
+		if (*s != c)
 			s++;
+		else
+			return (s);
 	}
-	if (*s == c)
+	if (c == '\0')
 		return (s);
-	else
+
 		return (NULL);
 }
