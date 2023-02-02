9f1d267d774786a7979f39b2abb98e18a1a315d0
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-02-02 05:48:28 -0800

    added a new element before a list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * *add_nodeint - function that adds a new node at the beginning
+ *                of a listint_t list.
+ * @head: current place in the  list
+ * @n: the int to be added at beginning of the list
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
